850bdbd1864891a123fda7c9d3436ccbc69ec955
Roxolana <roxolanapavlyk@gmail.com>
2019-03-26 13:54:37 +0100

    clean up component

--- imageapp/src/components/Image.js
+++ imageapp/src/components/Image.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import img from './images/img'
 import InfiniteScroll from "react-infinite-scroll-component"
+import ImageOne from './ImageOne'
+import Navigation from './Navigation'
+
+
 
 export default class Image extends Component {
     constructor(){
         super()
         this.state={
-            images: Array.from({length:5})
+            images: Array.from({length:5}), 
+         
         }
     }
     displayMore = () =>{
@@ -16,23 +21,28 @@ export default class Image extends Component {
             })
         }, 10000)
     }
+
+   
+
   render() {
-      let myImg = img.map(image => (<div style={image.style} ></div>))
+      let myImg = img.map(image => <ImageOne key={image.id} data={image.style}/>)
     return (
       <div> 
-    <InfiniteScroll
-    dataLength={this.state.images.length}
-    next={this.displayMore}
-    hasMore={false}
-    loader={<h2>Loading...</h2>}
-    endMessage={
-        <p style={{ textAlign: 'center' }}>
-        <b>Yay! You have seen it all</b>
-        </p>}>
-       <div className='wrapper'>
+            <InfiniteScroll
+                dataLength={this.state.images.length}
+                next={this.displayMore}
+                hasMore={false}
+                loader={<h2>Loading...</h2>}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>}>
+                    <Navigation/>
+                <div className='wrapper' >      
                 {myImg}
                 </div>
-       </InfiniteScroll>
+            </InfiniteScroll>
+  
       </div>
     )
   }
