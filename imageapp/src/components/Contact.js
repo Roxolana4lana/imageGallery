a9c330e62cf36dfffea9a9b9305e5d4eaca96134
Roxolana <roxolanapavlyk@gmail.com>
2019-03-27 16:08:14 +0100

    clean up component

--- imageapp/src/components/Contact.js
+++ imageapp/src/components/Contact.js
@@ -16,37 +16,41 @@ export default class Contact extends Component {
               />
           </GoogleMap>)
     return (
-      <div className='Contact'>
+      <div className='constactMain'>
       <Navigation/>
+        <div className='Contact'>
       <div className='contactInfo'>
-      <div className='contact_container_top'>
-     <h1>Modern art gallery</h1>
-          <p>phone: 097 555 555 555</p>
-          <p>wmail: gallery@gallery.com</p>
-                </div>
-                <div className='contact_container_medium'>
-    <h2>Opening hours</h2>
-                <p>During exhibitions:<br/>
-                      Tuesday – Friday | 12:00-18:00<br/>
-          Saturday | 12:00-16:00</p>
+            <div className='contact_container_top'>
+                <h1>Modern art gallery</h1>
+                <p>phone: 097 555 555 555</p>
+                <p>wmail: gallery@gallery.com</p>
             </div>
-                <div className='contact_container_bottom'>
-          <h2>Team</h2>
-                <p>Roksolana Pavlyk / owner /097 555 555 555</p>
-                <p>Roksolana Pavlyk / assistant /097 555 555 555</p>
-        </div>
-        <div className='contact_icons'>
-                    <i className="fab fa-instagram"></i>
-                    <i className="fab fa-facebook"></i>
-                    <i className="fab fa-twitter"></i>
-        </div>
+            <div className='contact_container_medium'>
+                <h2>Opening hours</h2>
+                <p>During exhibitions:<br/>
+                Tuesday – Friday | 12:00-18:00<br/>
+                Saturday | 12:00-16:00</p>
+            </div>
+            <div className='contact_container_bottom'>
+                <h2>Team</h2>
+                <p>Roksolana Pavlyk /owner</p><p>097 555 555 555</p>
+                        <p>Roksolana Pavlyk /assistant</p><p>097 555 555 555</p>
+            </div>
+                    <div className='contact_icons'>
+                        <i className="fab fa-instagram"></i>
+                        <i className="fab fa-facebook"></i>
+                        <i className="fab fa-twitter"></i>
+                    </div>
+            </div>
+                <MapWithAMarker
+                    containerElement={<div className='contactMap' />}
+                    mapElement={<div style={{ height: `100%`, width: '100%' }} />}
+                />
+                
+            
       </div>
-            <MapWithAMarker
-                containerElement={<div className='contactMap'  />}
-                mapElement={<div style={{ height: `100%`, width:'100%' }} />}
-            />
-        
-      </div>
+           
+        </div >
     )
   }
 }
