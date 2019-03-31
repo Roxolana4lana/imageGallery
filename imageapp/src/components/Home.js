4bb4472439907a5d0f8eebc981e0c6bac6400445
Roxolana <roxolanapavlyk@gmail.com>
2019-03-31 15:37:57 +0200

    changed url

--- imageapp/src/components/Home.js
+++ imageapp/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import NavigationMain from './NavigationMain'
-import image from './images/imageMain.jpeg'
+
 
 export default class Home extends Component {
   render() {
@@ -10,14 +10,14 @@ export default class Home extends Component {
         
       <div className='Home'>
                 <div className="section left" style={{
-                    backgroundImage: `url(${image})`, backgroundSize: 'cover',
+            backgroundImage: `url(https://images.pexels.com/photos/929282/pexels-photo-929282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`, backgroundSize: 'cover',
             backgroundPosition: 'center'}}>
                 
             </div>
             <div className='section right'>
                     <h1>Beauty in every detail...</h1>
                 <h2>Gallery is all about contemporary art, with work by <br/>
-                young artists or international artists rarely exhibited in the Ukraine</h2>
+                young artists or international artists rarely exhibited in Ukraine</h2>
             </div>
       </div>
         </React.Fragment>
--- imageapp/src/components/images/img.js
+++ imageapp/src/components/images/img.js
@@ -26,7 +26,7 @@ import pi25 from './pi25-min.jpg'
         'style':{
             gridColumn:'1/3',
             gridRow:'1/3',
-            background: `url(${pi1})`,
+            background: `url(https://images.pexels.com/photos/1061623/pexels-photo-1061623.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
            
@@ -38,7 +38,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '3/5',
             gridRow: '1/4',
-            background: `url(${pi2})`,
+            background: `url(https://images.pexels.com/photos/1038935/pexels-photo-1038935.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -49,7 +49,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '5/9',
             gridRow: '1/3',
-            background: `url(${pi3})`,
+            background: `url(https://images.pexels.com/photos/1386454/pexels-photo-1386454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -60,7 +60,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/3',
             gridRow: '3/5',
-            background: `url(${pi4})`,
+            background: `url(https://images.pexels.com/photos/957312/chess-checkmated-chess-pieces-black-white-957312.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -72,7 +72,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '5/9',
             gridRow: '3/5',
-            background: `url(${pi5})`,
+            background: `url(https://images.pexels.com/photos/220836/pexels-photo-220836.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -83,7 +83,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/3',
             gridRow: '7/9',
-            background: `url(${pi6})`,
+            background: `url(https://images.pexels.com/photos/573294/pexels-photo-573294.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -94,7 +94,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '3/5',
             gridRow: '4',
-            background: `url(${pi9})`,
+            background: `url(https://images.pexels.com/photos/910213/pexels-photo-910213.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -105,7 +105,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/4',
             gridRow: '5/7',
-            background: `url(${pi8})`,
+            background: `url(https://images.pexels.com/photos/114820/pexels-photo-114820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -116,7 +116,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '4/9',
             gridRow: '5/7',
-            background: `url(${pi7})`,
+            background: `url(https://images.pexels.com/photos/764880/pexels-photo-764880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -127,7 +127,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '3/5',
             gridRow: '7/9',
-            background: `url(${pi10})`,
+            background: `url(https://images.pexels.com/photos/53453/marilyn-monroe-woman-actress-pretty-53453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -138,7 +138,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '5/9',
             gridRow: '7/9',
-            background: `url(${pi11})`,
+            background: `url(https://images.pexels.com/photos/162389/lost-places-old-decay-ruin-162389.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -149,7 +149,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/4',
             gridRow: '9',
-            background: `url(${pi12})`,
+            background: `url(https://images.pexels.com/photos/270968/pexels-photo-270968.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -160,7 +160,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '4/7',
             gridRow: '9/11',
-            background: `url(${pi13})`,
+            background: `url(https://images.pexels.com/photos/399772/pexels-photo-399772.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -171,7 +171,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '7/9',
             gridRow: '9/11',
-            background: `url(${pi17})`,
+            background: `url(https://images.pexels.com/photos/211816/pexels-photo-211816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -182,7 +182,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/4',
             gridRow: '10',
-            background: `url(${pi15})`,
+            background: `url(https://images.pexels.com/photos/733881/pexels-photo-733881.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -193,7 +193,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/6',
             gridRow: '11',
-            background: `url(${pi16})`,
+            background: `url(https://images.pexels.com/photos/994883/pexels-photo-994883.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -204,7 +204,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '6/9',
             gridRow: '11/13',
-            background: `url(${pi18})`,
+            background: `url(https://images.pexels.com/photos/1573434/pexels-photo-1573434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -215,7 +215,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/6',
             gridRow: '12',
-            background: `url(${pi19})`,
+            background: `url(https://images.pexels.com/photos/18495/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -226,7 +226,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '1/5',
             gridRow: '13/15',
-            background: `url(${pi20})`,
+            background: `url(https://images.pexels.com/photos/235621/pexels-photo-235621.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
@@ -237,7 +237,7 @@ import pi25 from './pi25-min.jpg'
         'style': {
             gridColumn: '5/9',
             gridRow: '13/15',
-            background: `url(${pi25})`,
+            background: `url(https://images.pexels.com/photos/1044302/pexels-photo-1044302.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }
